16e433169e108a4233286bae718a4ae02a416a86
Rhys Franklin <Rhys.Franklin@fdmgroup.com>
2023-06-22 20:47:51 +0100

    format changes

--- src/main/java/com/community/meetup/service/ScheduleService.java
+++ src/main/java/com/community/meetup/service/ScheduleService.java
@@ -31,7 +31,7 @@ public class ScheduleService {
     public void scheduleMessage() throws Exception {
         log.info("sending scheduled message to slack");
         Organiser organiser = eventService.getNextOrganiser();
-        String message = "If you are interested in attending an upcoming social event please react with an emoji to express your interest, this month the person/persons responsible for organising the event is: " + organiser.getName() +  " :smiley:";
+        String message = "If you are interested in attending an upcoming social event please react with an emoji to express your interest, this month the person/persons responsible for organising the event is: " + organiser.getName() + " :smiley:";
         String ts = messageService.publishMessage(channelId, message);
         Event event = new Event(ts, organiser.getId());
         eventService.save(event);
@@ -41,11 +41,11 @@ public class ScheduleService {
     public void scheduleFollowUpMessage() throws Exception {
         log.info("sending follow up message to slack");
         Optional<String> optMessageId = eventService.getMostRecentMessageId();
-        if(optMessageId.isPresent()){
+        if (optMessageId.isPresent()) {
             String messageId = optMessageId.get();
             String token = System.getenv("SLACK_BOT_TOKEN");
             int numberOfInteractions = slackService.getNumberOfInteractions(messageId, channelId, token);
-            String message = numberOfInteractions + " people are interested in attending this months event. Please would he organiser for the next event propose a date for the event using the slack shortcut command /schedule-event ";
+            String message = numberOfInteractions + " people are interested in attending this months event. Please would the organiser for the next event propose a date for the event using the slack shortcut command /schedule-event ";
             messageService.publishMessage(channelId, message);
         } else {
             throw new Exception("no current events in database so could not produce follow up message");
